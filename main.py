e68d528f07cf986ec7c2989d2dfed89ecff087eb
Michael Aquilina <michaelaquilina@gmail.com>
2025-10-22 17:23:43 +0100

    support tro as an item action

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import requests
 from typing import Literal
+import functools
+from gi.repository import Gio
 
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
@@ -8,11 +10,29 @@ from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.action.BaseAction import BaseAction
 
 
 logger = logging.getLogger(__name__)
 
 SortBy = Literal["-created", "created", "-edited", "edited"]
+ItemAction = Literal["browser", "tro"]
+
+
+@functools.cache
+def get_default_terminal() -> tuple[str, str]:
+    settings = Gio.Settings.new("org.gnome.desktop.default-applications.terminal")
+    terminal = settings.get_string("exec")
+    terminal_args = settings.get_string("exec-arg")
+    return (terminal, terminal_args)
+
+
+def get_terminal_script(application: str) -> str:
+    terminal, exec_args = get_default_terminal()
+    script = f"{terminal} {exec_args} {application}"
+    logger.debug(f"{script=}")
+    return script
 
 
 def search(
@@ -20,6 +40,7 @@ def search(
     *,
     api_token: str,
     api_key: str,
+    item_action: ItemAction,
     default_sort: SortBy,
 ) -> list[ExtensionResultItem]:
     # By default, we assume the user is searching for open cards
@@ -36,6 +57,7 @@ def search(
         "key": api_key,
         "token": api_token,
         "cards_limit": "5",
+        "card_list": "true",
         "card_board": "true",
         "card_fields": "name,url",
     }
@@ -59,13 +81,27 @@ def search(
 
     for card in data["cards"]:
         description = ""
+        board_name = card["board"]["name"]
+        list_name = card["list"]["name"]
+        card_name = card["name"]
+
+        if item_action == "browser":
+            action = OpenUrlAction(card["url"])
+        elif item_action == "tro":
+            action = RunScriptAction(
+                get_terminal_script(
+                    f"tro show '{board_name}' '{list_name}' '{card_name}'"
+                )
+            )
+        else:
+            raise ValueError("Unable to determine action", item_action)
 
         results.append(
             ExtensionResultItem(
                 icon="images/icon.png",
-                name=f"[{card["board"]["name"]}] {card["name"]} ",
+                name=f"[{board_name}] {card_name}",
                 description=description,
-                on_enter=OpenUrlAction(card["url"]),
+                on_enter=action,
             )
         )
     return results
@@ -86,8 +122,10 @@ class KeywordQueryEventListener(EventListener):
         api_token = extension.preferences["api_token"]
         api_key = extension.preferences["api_key"]
         default_sort: SortBy = extension.preferences["default_sort"]
+        item_action: ItemAction = extension.preferences["item_action"]
 
         logger.debug(f"{default_sort=}")
+        logger.debug(get_default_terminal())
 
         argument = event.get_argument()
 
@@ -106,6 +144,7 @@ class KeywordQueryEventListener(EventListener):
             argument,
             api_token=api_token,
             api_key=api_key,
+            item_action=item_action,
             default_sort=default_sort,
         )
         return RenderResultListAction(results[:5])
